5f4b7522514cc88a431184ea524def06d3b4c385
linnefromice <linnefromice@gmail.com>
2021-03-07 18:31:46 +0900

    create main_app_scaffold

---
+++ lib/main_app_scaffold.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainAppScaffold extends StatefulWidget {
+  const MainAppScaffold({Key key, this.child}) : super(key: key);
+  final Widget child;
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MainAppScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RSS Feeder for zenn.dev',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: widget.child,
+    );
+  }
+}
