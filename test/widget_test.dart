11deceb9cf02b0044b017d7c8ba866cb5e060cc3
linnefromice <linnefromice@gmail.com>
2021-03-07 17:45:04 +0900

    update linter setting & fix some detections

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:linnefromice/main.dart';
 
 void main() {
-  testWidgets('Dummy', (WidgetTester tester) async {
+  testWidgets('Dummy', (tester) async {
     await tester.pumpWidget(App());
   });
 }
