0e72ca5df6470381f8f2eba8c10da10c7f66c683
linnefromice <linnefromice@gmail.com>
2021-03-07 18:38:51 +0900

    remove unnecessary code in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'views/home_page.dart';
+
 void main() {
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RSS Feeder for zenn.dev',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+  runApp(
+    HomePage()
+  );
 }
